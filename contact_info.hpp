a0c4ac26a2897d1a72e35f6aa6dffff1d8ff245d
Quinn McFarland <lilmcfarlands@gmail.com>
2026-06-11 14:10:54 -0700

    Define ContactInfo class in contact_info.hpp

--- contact_info.hpp
+++ contact_info.hpp
@@ -1,13 +1,30 @@
-//
-// Created by lilmc on 6/11/2026.
-//
+#ifndef CONTACT_INFO_HPP
+#define CONTACT_INFO_HPP
 
-#ifndef PERSONAL_INFORMATION_PROCESSOR_CONTACT_INFO_HPP
-#define PERSONAL_INFORMATION_PROCESSOR_CONTACT_INFO_HPP
+#include "address_info.hpp"
+#include <string>
+#include <iostream>
 
-
-class contact_info {
+class ContactInfo : public AddressInfo<std::string> {
+public:
+    ContactInfo() = default;
+    std::ostream& operator<<(std::ostream &out);
+    std::istream& operator>>(std::istream &in);
+    virtual void setFirstName(std::string data) { firstName = data; }
+    virtual void setLastName(std::string data) { lastName = data; }
+    void setHomePhone(std::string const &data) { homePhone = data; }
+    void setCellPhone(std::string const &data) { cellPhone = data; }
+    void setEmail(std::string const &data) { email = data; }
+    std::string getHomePhone() const { return homePhone; }
+    std::string getCellPhone() const { return cellPhone; }
+    std::string getEmail() const { return email; }
+protected:
+    std::string firstName;
+    std::string lastName;
+private:
+    std::string homePhone;
+    std::string cellPhone;
+    std::string email;
 };
 
-
-#endif //PERSONAL_INFORMATION_PROCESSOR_CONTACT_INFO_HPP
+#endif
