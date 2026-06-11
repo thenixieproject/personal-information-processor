1871bfeda488b6f136e8203027fa272c4566b8c8
Quinn McFarland <lilmcfarlands@gmail.com>
2026-06-11 14:23:25 -0700

    Define and implement PersonalInfo class.

---
+++ personal_info.cpp
@@ -0,0 +1,12 @@
+#include "personal_info.hpp"
+#include <iostream>
+
+std::ostream& PersonalInfo::operator<<(std::ostream &out) const {
+    out << firstName << lastName << hasContact << hasEducation;
+    return out;
+}
+
+std::istream& PersonalInfo::operator>>(std::istream &in) {
+    in >> firstName >> lastName >> hasContact >> hasEducation;
+    return in;
+}
--- personal_info.hpp
+++ personal_info.hpp
@@ -1,13 +1,28 @@
-//
-// Created by lilmc on 6/11/2026.
-//
+#ifndef PERSONAL_INFO_HPP
+#define PERSONAL_INFO_HPP
 
-#ifndef PERSONAL_INFORMATION_PROCESSOR_PERSONAL_INFO_HPP
-#define PERSONAL_INFORMATION_PROCESSOR_PERSONAL_INFO_HPP
+#include "contact_info.hpp"
+#include "education_info.hpp"
+#include <iostream>
+#include <string>
 
-
-class personal_info {
+class PersonalInfo : public ContactInfo, public EducationInfo {
+public:
+    PersonalInfo() = default;
+    std::ostream& operator<<(std::ostream &out) const;
+    std::istream& operator>>(std::istream &in);
+    void setFirstName(std::string data) override { firstName = data; }
+    void setLastName(std::string data) override { lastName = data; }
+    void setContact(bool data) { hasContact = data; }
+    void setEducation(bool data) { hasEducation = data; }
+    std::string getFirstName() const { return firstName; }
+    std::string getLastName() const { return lastName; }
+    bool getContact() const { return hasContact; }
+    bool getEducation() const { return hasEducation; }
+private:
+    bool hasEducation;
+    bool hasContact;
 };
 
 
-#endif //PERSONAL_INFORMATION_PROCESSOR_PERSONAL_INFO_HPP
+#endif
