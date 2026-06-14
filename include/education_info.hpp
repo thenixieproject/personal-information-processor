a8929adb7cca677bd83d66cfc0488ba3dae3255a
Quinn McFarland <lilmcfarlands@gmail.com>
2026-06-14 15:01:25 -0700

    Added address variables for school in EducationInfo class.

--- include/education_info.hpp
+++ include/education_info.hpp
@@ -23,6 +23,10 @@ private:
     bool graduated;
     int yearsAttended;
     std::string schoolName;
+    std::string schoolStreetAddress;
+    std::string schoolCity;
+    std::string schoolState;
+    std::string schoolZip;
 };
 
 #endif
